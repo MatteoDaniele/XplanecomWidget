f8eec5e30f638853e015a7d1f77ddf20664a944b
MatteoDaniele <matteodnl@gmail.com>
2018-12-19 14:54:16 +0100

    now it seems to work

--- XplanecomWidget.cpp
+++ XplanecomWidget.cpp
@@ -35,7 +35,12 @@
 // variables needed for the simulator
 float dt = -1;//0.006;
 float pi = 3.14159265359;
+float deg2rad = pi/180;
+float rad2deg = 1/deg2rad;
 
+// socket properties definition
+const char* Host = "127.0.0.1";
+int socketPort = 8888;
 
 char InstructionsText[50][200] = {
 "  Network options",
@@ -57,26 +62,25 @@ int gMenuItem;
 
 // initialize the datarefs
 XPLMDataRef OverrideFDMDataRef = NULL;
+XPLMDataRef OverrideJoystick = NULL;
 XPLMDataRef FlightModelReactivate = NULL;
 
 XPLMDataRef xLocal = NULL;
 XPLMDataRef yLocal = NULL;
 XPLMDataRef zLocal = NULL;
-/*
-XPLMDataRef phiFromDataRef = NULL;
-XPLMDataRef thetaFromDataRef = NULL;
-XPLMDataRef psiFromDataRef = NULL;
-*/
+
+XPLMDataRef phiDataRef = NULL;
+XPLMDataRef thetaDataRef = NULL;
+XPLMDataRef psiDataRef = NULL;
+
 // orientation given in quaternions
 XPLMDataRef quaternionVector = {NULL};
 
 // initialize socket and structure
 int socketInfo;
 struct sockaddr_in server;
-int receivedArray;
-// socket properties definition
-const char* Host;
-int socketPort;
+int receivedArray=1;
+
 // socket function declaration
 static float ReceiveDataFromSocket(
                                    float                inElapsedSinceLastCall,
@@ -110,19 +114,13 @@ double localX;
 double localY;
 double localZ;
 
-// for quaternion calculation
-float qVec[4];
-float phi1;
-float theta1;
-float psi1;
-
 // array to override internal fdm (20 values can be overridden, interested only in the first)
 int deactivateFlag[1];
 // array to reactivate internal fdm
 int reactivateFlag[1];
 // flag to check deactivation of fdm;
-int isFDMActiveFlag[1];
-int isFDMActive;
+//int isFDMActiveFlag[1];
+//int isFDMActive;
 // button state for the widget
 static XPWidgetID FDMbuttonState[1] = {NULL};
 
@@ -140,7 +138,8 @@ int CreateSocket(const char* host, int socketport){
     std::cerr<< "ERROR ON BINDING"<<'\n';
     return 1;
   }
-  std::cout << "Listening from IP address: "<< Host <<", port: "<< socketPort << '\n';
+  std::cout << "Listening from IP address: "<< Host <<", port: "<< socketPort <<" ..." << '\n';
+	return 0;
 }
 
 float ReceiveDataFromSocket(       float                inElapsedSinceLastCall,
@@ -179,21 +178,15 @@ float ReceiveDataFromSocket(       float                inElapsedSinceLastCall,
 	//transform in local coordinated that can be written as datarefs
 	XPLMWorldToLocal(latitude,longitude,elevation,&localX,&localY,&localZ);
 
-	// transform the desired flight angles in quaternions
-	phi1   = pi/360.0*phi;
-	theta1 = pi/360.0*theta;
-	psi1   = pi/360.0*psi;
-	qVec[0] =  cos(psi1) * cos(theta1) * cos(phi1) + sin(psi1) * sin(theta1) * sin(phi1);
-	qVec[1] =  cos(psi1) * cos(theta1) * sin(phi1) - sin(psi1) * sin(theta1) * cos(phi1);
-	qVec[2] =  cos(psi1) * sin(theta1) * cos(phi1) + sin(psi1) * cos(theta1) * sin(phi1);
-	qVec[3] = -cos(psi1) * sin(theta1) * sin(phi1) + sin(psi1) * cos(theta1) * cos(phi1);
-
 
 	// set the useful data
 	XPLMSetDatad(xLocal,localX);
 	XPLMSetDatad(yLocal,localY);
 	XPLMSetDatad(zLocal,localZ);
-	XPLMSetDatavf(quaternionVector,qVec,0,4);
+	XPLMSetDataf(phiDataRef,phi);
+	XPLMSetDataf(thetaDataRef,theta);
+	XPLMSetDataf(psiDataRef,psi);
+
 	return dt;
 
 }
@@ -211,9 +204,6 @@ PLUGIN_API int XPluginStart(
 	strcpy(outSig, "AttilaFrameSim.UDPplugin.Xplanecom");
 	strcpy(outDesc, "A plugin that disables Xplane's FDM, by Matteo Daniele");
 
-	// register flightloop callback
-	XPLMRegisterFlightLoopCallback(ReceiveDataFromSocket,dt,NULL);
-
 	// create the menu
 	// sub menu element (item of MenuXPlaneCom)
 	MenuItemXPlaneCom = XPLMAppendMenuItem(XPLMFindPluginsMenu(),"Internal FDM deactivator",NULL,1);
@@ -224,23 +214,28 @@ PLUGIN_API int XPluginStart(
 
 	/* NOTE: THE FOLLOWING LINES ARE RESPONSIBLE FOR THE DEACTIVATION
 		 OF THE INTERNAL FDM */
-	isFDMActiveFlag[0]=0;
-	isFDMActive = XPLMGetDatavi(OverrideFDMDataRef,isFDMActiveFlag,0,1);
+	//isFDMActiveFlag[0]=0;
+	//isFDMActive = XPLMGetDatavi(OverrideFDMDataRef,isFDMActiveFlag,0,1);
 	// get dataref handles when plugin starts
 	deactivateFlag[0] = 1;
 	OverrideFDMDataRef = XPLMFindDataRef("sim/operation/override/override_planepath");
+	OverrideJoystick = XPLMFindDataRef("sim/operation/override/override_joystick");
 	// find the 6 useful data
 	xLocal 	= XPLMFindDataRef("sim/flightmodel/position/local_x");
 	yLocal  = XPLMFindDataRef("sim/flightmodel/position/local_y");
 	zLocal  = XPLMFindDataRef("sim/flightmodel/position/local_z");
 
-	quaternionVector = XPLMFindDataRef("sim/flightmodel/position/q");
-	//TODO: better work with quaternions since euler angles are not reported so precisely
-	/*
-	phiFromDataRef 			= XPLMFindDataRef("sim/flightmodel/position/phi");
-	thetaFromDataRef 		= XPLMFindDataRef("sim/flightmodel/position/theta");
-	psiFromDataRef 			= XPLMFindDataRef("sim/flightmodel/position/psi");
-	*/
+	// euler angles
+	phiDataRef 	= XPLMFindDataRef("sim/flightmodel/position/phi");
+	thetaDataRef  = XPLMFindDataRef("sim/flightmodel/position/theta");
+	psiDataRef  = XPLMFindDataRef("sim/flightmodel/position/psi");
+
+
+
+
+	// register flightloop callback
+	XPLMRegisterFlightLoopCallback(ReceiveDataFromSocket,dt,NULL);
+
 	// flag tells us if the widget is displayed
 	gMenuItem = 0;
 	return 1;
@@ -255,10 +250,7 @@ PLUGIN_API void	XPluginStop(void)
 		XPDestroyWidget(ExternalFDMWidget,1);
 	}
 
-	//TODO: plugin stop OR plugin disable to reactivate the internal FDM?
-	reactivateFlag[0]=0;
-	XPLMSetDatavi(OverrideFDMDataRef,reactivateFlag,0,1);
-	close(socketInfo);
+
 	/* Unregister the callback */
 	XPLMUnregisterFlightLoopCallback(ReceiveDataFromSocket, NULL);
 
@@ -267,7 +259,12 @@ PLUGIN_API void	XPluginStop(void)
 PLUGIN_API void XPluginDisable(void) { }
 PLUGIN_API int  XPluginEnable(void)  { return 1; }
 PLUGIN_API void XPluginReceiveMessage(XPLMPluginID inFrom, int inMsg, void * inParam) { }
-
+/*
+float OverrideLoopCB(float elapsedMe, float elapsedSim, int counter, void * refcon)
+{
+	return 1;
+}
+*/
 void InstructionsMenuHandler(void * inMenuRef, void * inItemRef)
 {
 	switch ( (uintptr_t) inItemRef)
@@ -365,16 +362,25 @@ int	InstructionsHandler(XPWidgetMessage  inMessage, XPWidgetID  inWidget, long  
 	if (inMessage == xpMsg_ButtonStateChanged)
 	{
 
-		receivedArray = 1;
-		// here comes the override of the internal fdm
-		XPLMSetDatavi(OverrideFDMDataRef,deactivateFlag,0,1);
-		// socket properties definition
-		Host = "127.0.0.1";
-		socketPort = 8888;
+		if(XPGetWidgetProperty(FDMbuttonState[0],xpProperty_ButtonState,NULL)==1){
 		CreateSocket(Host, socketPort);
 		// start receiving data from socket
-  	// ReceiveDataFromSocket(Host,socketPort, receivedArray);
-		return 0;
+		// here comes the override of the internal fdm
+		XPLMSetDatavi(OverrideFDMDataRef,deactivateFlag,0,1);
+		// here comes joystick override
+		XPLMSetDatai(OverrideJoystick,1);
+
+
+  	return 0;
+	}/*
+		// reactivate the internal fdm and the close the socket when the button is pushed again
+		else{
+			//TODO: plugin stop OR plugin disable to reactivate the internal FDM?
+			reactivateFlag[0]=0;
+			XPLMSetDatavi(OverrideFDMDataRef,reactivateFlag,0,1);
+			close(socketInfo);
+			return 0;
+		}*/
 	}
 
 	return 0;
